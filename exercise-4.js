44af4ab644c53c9c3dcfc33c621e551f681b94a3
dani <iafgon@yahoo.com>
2019-01-07 12:57:57 +0700

    fix error

--- exercise-4.js
+++ exercise-4.js
@@ -1,6 +1,6 @@
---------------
+//--------------
 //switch-case|
---------------
+//--------------
 var tanggal = 31; // assign nilai variabel tanggal disini! (dengan angka antara 1 - 31)
 var bulan = 12; // assign nilai variabel bulan disini! (dengan angka antara 1 - 12)
 var tahun = 1986; // assign nilai variabel tahun disini! (dengan angka antara 1900 - 2200)
